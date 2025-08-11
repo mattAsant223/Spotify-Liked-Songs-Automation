a2d6ab67bf4ec0811afc446e2daebd84ccbe62b2
matt89 <mkaj@utexas.edu>
2025-08-11 15:57:26 -0500

    accelerated runtime speed for processing songs in playlist

--- Spotify Liked Songs main.py
+++ Spotify Liked Songs main.py
@@ -2,10 +2,11 @@ import requests
 import base64
 from urllib.parse import urlencode
 import webbrowser
+import time
 
 # most of these are generated once you fill out the spotify web api app and create it for your project
 # your playistid is located in the url when you create a playlist, or you can use api as well!
-from Secret import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, PLAYLIST_ID
+from Liked_Songs_Secret import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, PLAYLIST_ID
 
 
 # this function retrieves a token for us to be authorized to let spotify get information within the
@@ -57,50 +58,26 @@ def get_playlist_tracks(token, playlist_id):
     # classic fencing problem. calculate how many times we iterate by 50 tracks then deal with
     # grabbing the leftovers later.
     playlistTotal = playlistTracksJson["total"]
-    modularPlaylistTotal = playlistTotal % 50
-    playListIterativeSteps = playlistTotal // 50
+    return playlistTotal
 
-    # loop through 50 tracks at a time through updating the url's offset variable by 50 to get the
-    # 50 next tracks
-    for i in range(0, playListIterativeSteps):
-        url = ("https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?offset=" +
-               str(offsetVariable) + "&limit=50")
-        playlistTracks = requests.get(url, headers=headers)
-        playlistTracksJson = playlistTracks.json()
 
-        for j in range(0, 50):
-            playListUris.append(str(playlistTracksJson["items"][j]["track"]["uri"]))
-        offsetVariable += 50
-
-    # once the loop is finished, add those last few tracks that we
-    # identified through getting the remainder
-    url = ("https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?offset=" +
-           str(offsetVariable) + "&limit=50")
-    playlistTracks = requests.get(url, headers=headers)
-    playlistTracksJson = playlistTracks.json()
-    for k in range(0, modularPlaylistTotal):
-        playListUris.append(str(playlistTracksJson["items"][k]["track"]["uri"]))
-
-    # return the list to now compare what needs to be added to the playlist!
-    return playListUris
-
+    
 
 # get the songs in the liked playlist
 
 # same idea as the previous function but now with the actual liked songs collection
-def get_songs(token, playlistUris):
-    # start off with empty list that we will add the spotify uri's to
-    trackuris = []
+def get_songs(token, playlistTotal):
+
     # keep track also of how much were offsetting to grab every track possible
     offsetVariable = 0
     # plug in offset variable in the url, and we want the limit to be as large as possible
     # spotify api's max is 50
-    urll = "https://api.spotify.com/v1/me/tracks?offset=" + str(offsetVariable) + "&limit=50"
+    urll = "https://api.spotify.com/v1/me/tracks?offset=" + str(0) + "&limit=50"
     headers = get_auth_header(token)
     tracks = requests.get(urll, headers=headers)
     tracksJson = tracks.json()
     # create variable to get amount of tracks in liked songs collection
-    likedSongsTotal = tracksJson["total"]
+    likedSongsTotal = tracksJson["total"] - playlistTotal
     # create variable to see whatever the total is remainder 50, so we know how many songs are left over
     # after the last iterative 50 step
     modularTotal = likedSongsTotal % 50
@@ -110,37 +87,37 @@ def get_songs(token, playlistUris):
 
     # loop through 50 tracks at a time through updating the url's offset variable by 50 to get the
     # 50 next tracks
+    trackStack = []
+    # use a stack to conviently keep order of the tracks we want to add and in the 50 song increments as well
     for i in range(0, iterativeSteps):
         urll = "https://api.spotify.com/v1/me/tracks?offset=" + str(offsetVariable) + "&limit=50"
         tracks = requests.get(urll, headers=headers)
         tracksJson = tracks.json()
-
+        trackuris = []
         for j in range(0, 50):
-            # important conditional: if this uri is already in the playlist, don't add it!
-            if tracksJson["items"][j]["track"]["uri"] in playlistUris:
-                continue
-            else:
-                trackuris.append(str(tracksJson["items"][j]["track"]["uri"]))
+            trackuris.append(str(tracksJson["items"][j]["track"]["uri"]))
+        trackStack.append(trackuris)
         offsetVariable += 50
 
     # once the loop is finished, add those last few tracks that we identified through getting the remainder
     urll = "https://api.spotify.com/v1/me/tracks?offset=" + str(offsetVariable) + "&limit=50"
     tracks = requests.get(urll, headers=headers)
     tracksJson = tracks.json()
-    for k in range(0, modularTotal):
-        # same conditional but for the leftover tracks
-        if tracksJson["items"][k]["track"]["uri"] in playlistUris:
-            continue
-        else:
-            trackuris.append(str(tracksJson["items"][k]["track"]["uri"]))
+    if (modularTotal != 0):
 
+        trackuris = []
+        for k in range(0, modularTotal):
+
+            # same idea but now we are only getting the leftover tracks
+            trackuris.append(str(tracksJson["items"][k]["track"]["uri"]))
+        trackStack.append(trackuris)
     # return the list to now populate the playlist!
-    return trackuris
+    return trackStack
 
 
 # populating the playlist: parameters are the list of trackuris we created from get_songs,
 # the playlist id, the token we were authorized with, and the list of tracks from the playlist
-def populate_playlists(token, PLAYLIST_ID, playlistUris, trackuris):
+def populate_playlists(token, PLAYLIST_ID, playlistTotal, trackStack):
     uriString = ""
     url = "https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString
     headers = get_auth_header(token)
@@ -151,16 +128,14 @@ def populate_playlists(token, PLAYLIST_ID, playlistUris, trackuris):
         "Authorization": "Bearer {}".format(token),
     }
 
-    # once again grab how many tracks we have from the previous function
-    spliceTotal = len(trackuris)
-    spliceModulo = spliceTotal % 50
-    spliceTotal //= 50
+    # we can now just pop the trackStack and add the tracks in increments of 50
+    # we do this so we don't have to worry about how many tracks are in the liked
+    # songs collection, nor the order since the stack maintains it
 
-    # one based indexing, because multiplying by 0 is always 0
-    for i in range(1, spliceTotal + 1):
+    while len(trackStack) != 0:
         # create substrings of the list of uris because spotify only takes so much at once,
         # in this case 50 was a workable number for spotify
-        subTrackuri = trackuris[(i - 1) * 50:i * 50]
+        subTrackuri = trackStack.pop()
         # join each uri with %2C and replace colons with %3A
         subTrackuriString = "%2C".join(subTrackuri)
         while subTrackuriString.find(":") != -1:
@@ -168,12 +143,8 @@ def populate_playlists(token, PLAYLIST_ID, playlistUris, trackuris):
 
         uriString = subTrackuriString
         # update the url with the new uriStrings and continue to loop through the tracks!
-
-        if len(playlistUris) != 0:
-            url = ("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString
-                   + "&position=0")
-        else:
-            url = ("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString)
+        url = ("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString
+               + "&position=0")
 
         response = requests.post(url, headers=head)
         if response.status_code == 201:
@@ -181,32 +152,22 @@ def populate_playlists(token, PLAYLIST_ID, playlistUris, trackuris):
         else:
             print(f'Failed to add tracks: {response.status_code} - {response.text}')
 
-    # exact same idea but with the leftover tracks
-    # clever way to get the last few tracks without having to keep track of
-    # how many splices we've made. We know because of our remainder calculation it will be this
-    # amount of tracks everytime
-    subTrackuri = trackuris[(-spliceModulo):]
-    subTrackuriString = "%2C".join(subTrackuri)
-    while subTrackuriString.find(":") != -1:
-        subTrackuriString = subTrackuriString.replace(":", "%3A")
-    uriString = subTrackuriString
-    # small detail, if the playlist already had tracks, and were only
-    # adding a few tracks that doesn't exceed the remainder, we send the latest ones to the top
-    if len(playlistUris) != 0:
-        url = ("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString
-               + "&position=0")
-    else:
-        url = ("https://api.spotify.com/v1/playlists/" + PLAYLIST_ID + "/tracks?uris=" + uriString)
 
-    response = requests.post(url, headers=head)
-    if response.status_code == 201:
-        print('Tracks added successfully')
-    else:
-        print(f'Failed to add tracks: {response.status_code} - {response.text}')
 
 
 # run what we have! enjoy!
 token = get_token()
+playlistParseTime = time.perf_counter()
 playlistSongs = get_playlist_tracks(token, PLAYLIST_ID)
+playlistParseTimeend = time.perf_counter()
+print("time for parsing original automated playlist: " + str(playlistParseTimeend - playlistParseTime))
+likedSongsParseTime = time.perf_counter()
 song_list = get_songs(token, playlistSongs)
+likedSongsParseTimeend = time.perf_counter()
+print("time for parsing liked songs collection: " + str(likedSongsParseTimeend - likedSongsParseTime))
+postingSongsParseTime = time.perf_counter()
 populate_playlists(token, PLAYLIST_ID, playlistSongs, song_list)
+postingSongsParseTimeend = time.perf_counter()
+print("time for populating automated playlist: " + str(postingSongsParseTimeend - postingSongsParseTime))
+
+
